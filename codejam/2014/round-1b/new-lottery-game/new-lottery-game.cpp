c330a07b425f10ca86fbf65daa288995c0a1339a
Tiberiu Popa <popa.tiberiu@gmail.com>
2014-05-11 13:48:41 +0100

    Added Part Elf from Code Jam 2014 - Round 1C.

--- codejam/2014/round-1b/new-lottery-game/new-lottery-game.cpp
+++ codejam/2014/round-1b/new-lottery-game/new-lottery-game.cpp
@@ -10,7 +10,7 @@ using namespace std;
 template<typename T>
 int get_bit(T n, int bit)
 {
-	return (n & (1 << bit)) != 0;
+  return (n & (1 << bit)) != 0;
 }
 
 template<typename T>
@@ -57,118 +57,118 @@ T ipow(T x, int e)
 
 int ms_different_bit(int n, int m)
 {
-	return get_msb(n ^ m);
+  return get_msb(n ^ m);
 }
 
 void split_interval(int n, vector< pair<int, int> > &v)
 {
-	v.clear();
-	v.push_back(make_pair(0, 0));
-	int x = 1;
-	while (1) {
-		int y = x << 1;
-		if (y > n)
-			break;
-		v.push_back(make_pair(x, y - 1));
-		x = y;
-	}
-	for (int t = x >> 1; t > 0; t >>= 1) {
-		int y = x + t;
-		if (y <= n) {
-			v.push_back(make_pair(x, y - 1));
-			x = y;
-		}
-	}
+  v.clear();
+  v.push_back(make_pair(0, 0));
+  int x = 1;
+  while (1) {
+    int y = x << 1;
+    if (y > n)
+      break;
+    v.push_back(make_pair(x, y - 1));
+    x = y;
+  }
+  for (int t = x >> 1; t > 0; t >>= 1) {
+    int y = x + t;
+    if (y <= n) {
+      v.push_back(make_pair(x, y - 1));
+      x = y;
+    }
+  }
 }
 
 long long solve_subcase(pair<int, int> u, pair<int, int> v, pair<int, int> w)
 {
-	long long s = 0;
-	int pw2 = 0;
-	int pw3 = 0;
+  long long s = 0;
+  int pw2 = 0;
+  int pw3 = 0;
 
-	int x = ms_different_bit(u.first, u.second);
-	int y = ms_different_bit(v.first, v.second);
-	int z = ms_different_bit(w.first, w.second);
+  int x = ms_different_bit(u.first, u.second);
+  int y = ms_different_bit(v.first, v.second);
+  int z = ms_different_bit(w.first, w.second);
 
-	int upper_bit = max(x, y);
-	int lower_bit = min(x, y);
+  int upper_bit = max(x, y);
+  int lower_bit = min(x, y);
 
-	int highest_bit = max(z, upper_bit);
-	int left_root = (u.first & v.first) >> highest_bit;
-	int right_root = w.first >> highest_bit;
-	if (left_root != right_root) {
-		return s;
-	}
+  int highest_bit = max(z, upper_bit);
+  int left_root = (u.first & v.first) >> highest_bit;
+  int right_root = w.first >> highest_bit;
+  if (left_root != right_root) {
+    return s;
+  }
 
-	int lol = (x < y) ? u.first : v.first;
-	for (int i = upper_bit - 1; i >= 0; i--) {
-		if (i >= lower_bit) {
-			if (i >= z) {
-				int operand_bit = get_bit(lol, i);
-				int result_bit = get_bit(w.first, i);
-				if (!operand_bit) {
-					if (result_bit) {
-						return s;
-					} else {
-						++pw2;
-					}
-				}
-			} else {
-				++pw2;
-			}
-		} else {
-			if (i >= z) {
-				int result_bit = get_bit(w.first, i);
-				if (!result_bit) {
-					++pw3;
-				}
-			} else {
-				pw2 += 2;
-			}
-		}
-	}
+  int lol = (x < y) ? u.first : v.first;
+  for (int i = upper_bit - 1; i >= 0; i--) {
+    if (i >= lower_bit) {
+      if (i >= z) {
+        int operand_bit = get_bit(lol, i);
+        int result_bit = get_bit(w.first, i);
+        if (!operand_bit) {
+          if (result_bit) {
+            return s;
+          } else {
+            ++pw2;
+          }
+        }
+      } else {
+        ++pw2;
+      }
+    } else {
+      if (i >= z) {
+        int result_bit = get_bit(w.first, i);
+        if (!result_bit) {
+          ++pw3;
+        }
+      } else {
+        pw2 += 2;
+      }
+    }
+  }
 
-	s = 1LL << pw2;
-	s *= ipow(3LL, pw3);
+  s = 1LL << pw2;
+  s *= ipow(3LL, pw3);
 
-	return s;
+  return s;
 }
 
 int solve_problem(int num_case)
 {
-	vector< pair<int, int> > u, v, w;
-	int a, b, k;
+  vector< pair<int, int> > u, v, w;
+  int a, b, k;
 
-	if (scanf("%d %d %d", &a, &b, &k) != 3)
-		return 1;
+  if (scanf("%d %d %d", &a, &b, &k) != 3)
+    return 1;
 
-	split_interval(a, u);
-	split_interval(b, v);
-	split_interval(k, w);
+  split_interval(a, u);
+  split_interval(b, v);
+  split_interval(k, w);
 
-	long long result = 0;
-	for (size_t i = 0; i < u.size(); i++) {
-		for (size_t j = 0; j < v.size(); j++) {
-			for (size_t k = 0; k < w.size(); k++) {
-				result += solve_subcase(u[i], v[j], w[k]);
-			}
-		}
-	}
+  long long result = 0;
+  for (size_t i = 0; i < u.size(); i++) {
+    for (size_t j = 0; j < v.size(); j++) {
+      for (size_t k = 0; k < w.size(); k++) {
+        result += solve_subcase(u[i], v[j], w[k]);
+      }
+    }
+  }
 
-	printf("Case #%d: %lld\n", num_case, result);
+  printf("Case #%d: %lld\n", num_case, result);
 
-	return 0;
+  return 0;
 }
 
 int main()
 {
-	int num_tests;
+  int num_tests;
 
-	if (scanf("%d", &num_tests) != 1)
-		return 1;
-	for (int i = 0; i < num_tests; i++)
-		solve_problem(i + 1);
+  if (scanf("%d", &num_tests) != 1)
+    return 1;
+  for (int i = 0; i < num_tests; i++)
+    solve_problem(i + 1);
 
-	return 0;
+  return 0;
 }
--- codejam/2014/round-1b/repeater/repeater.cpp
+++ codejam/2014/round-1b/repeater/repeater.cpp
@@ -21,90 +21,90 @@ char *trim_line(char *str)
 
 bool compare_vectors(const vector< pair<int, int> > &x, const vector< pair<int, int> > &y)
 {
-	if (x.size() != y.size()) {
-		return false;
-	}
-	for (size_t i = 0; i < x.size(); i++) {
-		if (x[i].first != y[i].first) {
-			return false;
-		}
-	}
-	return true;
+  if (x.size() != y.size()) {
+    return false;
+  }
+  for (size_t i = 0; i < x.size(); i++) {
+    if (x[i].first != y[i].first) {
+      return false;
+    }
+  }
+  return true;
 }
 
 int solve_problem(int num_case)
 {
-	int n;
+  int n;
 
-	if (fgets(str, sizeof(str), stdin) == NULL)
-		return 1;
-	n = atoi(str);
+  if (fgets(str, sizeof(str), stdin) == NULL)
+    return 1;
+  n = atoi(str);
 
-	for (int i = 0; i < n; i++) {
-		if (fgets(str, sizeof(str), stdin) == NULL)
-			return 1;
-		trim_line(str);
-		v[i].clear();
-		size_t len = strlen(str);
-		int last_ch = -1;
-		int cnt = 0;
-		for (size_t j = 0; j < len; j++) {
-			int ch = str[j] - 'a';
-			if (ch != last_ch) {
-				if (last_ch >= 0) {
-					v[i].push_back(make_pair(last_ch, cnt));
-				}
-				last_ch = ch;
-				cnt = 1;
-			} else {
-				++cnt;
-			}
-		}
-		if (last_ch >= 0) {
-			v[i].push_back(make_pair(last_ch, cnt));
-		}
-	}
+  for (int i = 0; i < n; i++) {
+    if (fgets(str, sizeof(str), stdin) == NULL)
+      return 1;
+    trim_line(str);
+    v[i].clear();
+    size_t len = strlen(str);
+    int last_ch = -1;
+    int cnt = 0;
+    for (size_t j = 0; j < len; j++) {
+      int ch = str[j] - 'a';
+      if (ch != last_ch) {
+        if (last_ch >= 0) {
+          v[i].push_back(make_pair(last_ch, cnt));
+        }
+        last_ch = ch;
+        cnt = 1;
+      } else {
+        ++cnt;
+      }
+    }
+    if (last_ch >= 0) {
+      v[i].push_back(make_pair(last_ch, cnt));
+    }
+  }
 
-	int result = 0;
-	for (int i = 1; i < n; i++) {
-		if (!compare_vectors(v[0], v[i])) {
-			result = -1;
-			break;
-		}
-	}
-	if (result >= 0) {
-		size_t len = v[0].size();
-		for (size_t i = 0; i < len; i++) {
-			int s = numeric_limits<int>::max();
-			for (int j = 0; j <= MAX_LENGTH; j++) {
-				int cand = 0;
-				for (int k = 0; k < n; k++) {
-					cand += abs(v[k][i].second - j);
-				}
-				if (cand < s) {
-					s = cand;
-				}
-			}
-			result += s;
-		}
-		printf("Case #%d: %d\n", num_case, result);
-	} else {
-		printf("Case #%d: %s\n", num_case, "Fegla Won");
-	}
+  int result = 0;
+  for (int i = 1; i < n; i++) {
+    if (!compare_vectors(v[0], v[i])) {
+      result = -1;
+      break;
+    }
+  }
+  if (result >= 0) {
+    size_t len = v[0].size();
+    for (size_t i = 0; i < len; i++) {
+      int s = numeric_limits<int>::max();
+      for (int j = 0; j <= MAX_LENGTH; j++) {
+        int cand = 0;
+        for (int k = 0; k < n; k++) {
+          cand += abs(v[k][i].second - j);
+        }
+        if (cand < s) {
+          s = cand;
+        }
+      }
+      result += s;
+    }
+    printf("Case #%d: %d\n", num_case, result);
+  } else {
+    printf("Case #%d: %s\n", num_case, "Fegla Won");
+  }
 
-	return 0;
+  return 0;
 }
 
 int main()
 {
-	int num_tests;
+  int num_tests;
 
-	if (fgets(str, sizeof(str), stdin) == NULL)
-		return 1;
-	num_tests = atoi(str);
+  if (fgets(str, sizeof(str), stdin) == NULL)
+    return 1;
+  num_tests = atoi(str);
 
-	for (int i = 0; i < num_tests; i++)
-		solve_problem(i + 1);
+  for (int i = 0; i < num_tests; i++)
+    solve_problem(i + 1);
 
-	return 0;
+  return 0;
 }
